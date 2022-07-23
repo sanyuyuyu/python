32c8bbc022e322abb719b239430df20c5f6a9e9d
sanyuyuyu <1733659500@qq.com>
2022-07-23 23:16:06 +0800

    加了寂寞

---
+++ test2.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Date:   2022-07-23 22:45:42
+# @Last Modified time: 2022-07-23 23:13:05
+print('hello world')
+print('200+300=',200+300)
+name = input('please enter your name:')
+print('hello,',name)
+
+a=100
+if 0<=a<1
+   print("a1",a)
+elif 1<=a<2
+   print("a2",a)
+elif i>3
+   print("a3",a)
+else
+   print(-a)
+
+
+ def sum1(n:int):
+    return ((n+1)*n)//2
+
+
+def sun_of_a_and_b(a:int,b:int):
+    return (a+b)
+
+def command(a1:str,a2:str):
+    return (a1+a2)
+print(command('chi'+'na'))
+
+
+def text1(a1:int,a2:int,a3:int):
+    return(a1*a2+a3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
