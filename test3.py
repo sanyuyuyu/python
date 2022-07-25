7b236d4fecefdca91971340da683deeec1448cb0
sanyuyuyu <1733659500@qq.com>
2022-07-25 20:17:18 +0800

    test4

---
+++ test3.py
@@ -0,0 +1,92 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Date:   2022-07-24 13:14:35
+# @Last Modified time: 2022-07-24 21:44:20
+true and false
+
+
+age = 16
+if age >= 18:
+    print('adult')
+else:
+    print('teenager')
+
+
+print('i\'m ok')
+print("i'm ok")
+
+a = 'abd'
+b = a
+a = 'xye'
+print(b)
+
+ord('A')
+
+
+def beauty(y: int,z: int):
+    return y*z
+
+
+b'ABC'.decode('ascii')
+
+
+len('beauty')
+
+
+"chinese".encode('utf-8')
+
+
+print('%d2-%02' % (3,1))
+
+
+print('%.f2' % 3.1415926)
+
+
+'Age: %s. Gender: %s' % (18,true)
+
+
+'growth rate: %d %%' % 7
+
+
+zoo =  ['monkey', 'dog', snake')
+
+
+temp = input ('please guess a number')
+guess = int(temp)
+
+
+if guess == 8:
+    print('you are good')
+    print('perfect')
+else:
+    print('it is 8')
+print('end')
+
+
+print("let's go!go!go!")
+
+
+print('chinese.\nausia')
+
+
+
+print("D:\\three\\abandan\\now")
+
+
+
+temp = input()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test4.py
@@ -0,0 +1,103 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Date:   2022-07-25 13:55:57
+# @Last Modified time: 2022-07-25 19:56:52
+
+
+
+print(r'D:three\two\one\abandon')
+
+
+print('D:three\two\one\abandon')
+
+
+
+counts = 3
+while counts > 0:
+   print('you are beautiful')
+   counts = counts - 1
+
+
+counts = 8
+while counts > 0:
+
+temp = input('please guess my heart number:')
+guess = int(temp)
+
+if guess == 8:
+    print('you are very good')
+    print('beautiful')
+else:
+    if counts < 8:
+        print('too low')
+    
+    else:
+        print('too big')
+    counts = counts - 1
+
+print('end')
+
+
+random.randint(1,100000)
+
+
+i = 0
+while i < 1
+    i = i + 1
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
