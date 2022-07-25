03158452dc59dfa59f9ebb2231ace8b239787c89
sanyuyuyu <1733659500@qq.com>
2022-07-25 22:28:53 +0800

    test4

--- test4.py
+++ test4.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 # @Date:   2022-07-25 13:55:57
-# @Last Modified time: 2022-07-25 19:56:52
+# @Last Modified time: 2022-07-25 21:54:55
 
 
 
@@ -18,9 +18,59 @@ while counts > 0:
    counts = counts - 1
 
 
-counts = 8
-while counts > 0:
 
+
+random.randint(1,100000)
+
+
+i = 0
+while i < 1
+    i = i + 1
+    print(i)
+
+
+divmod(3,2)
+
+
+
+i = 1
+sum =0
+while i < 100
+    sum = sum + i
+    i = i + 1
+print(sum)
+
+
+
+if 3 < 2:
+   print(true)
+print(false)
+
+
+
+if 3 < 4:
+    print(false)
+else:
+    print(true)
+
+
+old = input("please tell me your old:")
+old = int(old)
+
+if 0 < old =18:
+    print('teenager')
+elif 18 > old > 40:
+    print('adult')
+elif 40 <= 90:
+    print('older')
+else:
+    print('what')
+
+
+
+
+counts = 8
+while counts
 temp = input('please guess my heart number:')
 guess = int(temp)
 
@@ -36,18 +86,44 @@ else:
     counts = counts - 1
 
 print('end')
+ 
 
 
-random.randint(1,100000)
+a = 3
+b = 4
+if a < b:
+    small = a
+else:
+    small = b
 
 
-i = 0
-while i < 1
-    i = i + 1
-    print(i)
+small = a if a < b else b
 
 
 
+old = input("please tell me your old:")
+old = int(old)
+
+if 0 < old <=18:
+    print('teenager')
+elif 18 > old > 40:
+    print('adult')
+elif 40 <= 90:
+    print('older')
+else:
+    print('what')
+
+
+
+old = 18
+rank = ('teenager' if 0 < old <18
+        'adult' if 18 < old < 40
+        'older' if 41 < old < 90 )
+
+while true:
+    answer = input('please let me ddone')
+    if answer == 'yes':
+         break
 
 
 
