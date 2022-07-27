ca6257017fec305732b78652229853c82ddc03f2
sanyuyuyu <1733659500@qq.com>
2022-07-27 22:46:02 +0800

    test6

--- test6.py
+++ test6.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 # @Date:   2022-07-27 14:12:44
-# @Last Modified time: 2022-07-27 15:43:13
+# @Last Modified time: 2022-07-27 22:43:46
 x = [1,2,3,4]
 y = x
 x[1] = 1
@@ -9,18 +9,108 @@ x = [[1,2,3],[4,5,6],[7,8,9
 
 
 
-
-
-oho = [1,2,3,4]
-for i in range(len(oho)):
-    oh[i] = oh[i] * 2
-
+x = 3
+if x <= 4:
+    print('it is a samall number')
+  
 
 y = copy.copy(x)
 y = copy.deepcopy()
 
 
 
-
 x = [i for i in range(10)]
 x = [i + 1 for i in range(10)]
+
+
+
+y = [c * 2 for c in 'fish']
+
+
+
+code = [ord(c) for c in "fishc"]
+
+
+matrix = [[1,2,3],
+          [4,5,6],
+          [7,8,9]]
+col2 = [row[1] for row in matrix
+col2]b
+
+
+
+
+even = [i for i in range(10) if i % 10 == 0]
+
+
+words = ['great','fish','fantistic','big','tiger']
+fwards = [w for w in words if w[0] == 'f' ]
+fwords
+
+
+
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+flatten = [col for row in matrix for col in row]
+flatten
+
+
+flatten []
+for row in matrix:
+    col in row:
+    flatten.append(col)
+
+
+[x + y for x in "fishc" for y in "fishc"]
+
+
+for x in "fishc":
+    for y in "fishc":
+        _.append(x + y)
+
+
+[[x,y] for x in range(10) if x % 2 ==0 foe y in range(10) if y % 3 == 0]
+
+
+
+
+for x in range(10):
+    if x % 2 == 0:
+        for y in range(10):
+            if y % 3 == 0:
+                _append(x,y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
