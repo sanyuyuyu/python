755ceae246c6f7bda0ca40e290671bb889d900de
sanyuyuyu <1733659500@qq.com>
2022-07-28 21:12:22 +0800

    test7

---
+++ test7.py
@@ -0,0 +1,125 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Date:   2022-07-28 19:55:34
+# @Last Modified time: 2022-07-28 21:12:03
+
+
+rhyme = (1,2,3,4,'you are beautiful')
+rhyme = [:3]
+nums = (3,4,5,7,9,4)
+nums.count(3)
+heros.index.
+
+
+
+w = (1,2,3)
+s = (4,5,6)
+w + s
+y = w , s
+for each in s:
+    print(each)
+
+for each in w:
+    print(each)
+
+s = (3,4,5,2,2)
+[each * 2 for each in s]
+
+
+B[1][1] = 1
+a = [0] * 3
+for i in range(3)
+
+
+rhyme[-1]
+
+
+(each * 2 for each in s)
+
+
+x = （520）
+type(x)
+
+
+x = (520,)
+t = (123,"fishc",3.14)
+t = [123,"fishc",3.14]
+x, y, z = t 
+
+
+a,b,c = "fishc"
+a,b,*c = "fishc"
+
+
+w[0][0]
+
+
+x = "12321"
+'it is back number' if x == [::-1] else "it is not back number"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
