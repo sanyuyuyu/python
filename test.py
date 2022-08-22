4ba2cbcc8e7d75473565bedc8c732fd6f1f32def
sanyuyuyu <1733659500@qq.com>
2022-08-22 21:33:53 +0800

    test_20220822

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 # @Date:   2022-08-07 17:57:35
-# @Last Modified time: 2022-08-21 21:12:59
+# @Last Modified time: 2022-08-22 21:31:12
 
 
 file_object = open("test.txt",'r') #创建一个文件对象，也是一个可迭代对象
@@ -71,6 +71,60 @@ import numpy as np
 #将test.txt文件中的内容写入data.txt文件
 with open('data.txt','ab') as f:
     f.write(open('test.txt','rb').read())
+
+
+#读取文件
+with open("./test.txt") as file_object:
+    content = file_object.read() #file_object.read() 用于读取文件内容，以str形式储存在变量content里
+    print(content)
+
+#逐行读取
+file_name = "test.txt"
+
+with open(file_name) as file_object:
+    for line in file_object:
+        print(line)       # 此时行与行之间会有空行，因为打印了换行符
+        print(line.rstrip())    # 此时没有空行
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
