117c1f5d466e058cacf45dd0ec99febddaae9def
sanyuyuyu <1733659500@qq.com>
2022-08-30 22:04:27 +0800

    test_20220830

---
+++ test25.py
@@ -0,0 +1,64 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Date:   2022-08-30 21:09:00
+# @Last Modified time: 2022-08-30 22:03:46
+my_str = "absdufro"
+new_my_str = my_str.title()  #字标索引值
+print(new_my_str)
+#title 字母开头大写
+#lower转小写
+#strip 去除左右两端空格
+#upper转大写
+
+# my_str.count(sub_str[, start[, end]])
+
+print("abcdesfsw".count('a'))
+
+# my_str.replace(old, new[, count])  old替换为new新字符串
+
+my_str = "i am beautiful"
+my_str.split(sep=None, maxsplit=3)
+print(my_str)
+
+
+my_str = 'is it ok ?'
+answer_str = "is it ok ?: {}".format("yes.")
+print(answer_str)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
