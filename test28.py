ff135317404effb9d2f6fb68cee6366491ed2c5e
sanyuyuyu <1733659500@qq.com>
2022-09-03 21:04:37 +0800

    test_20220903

--- test28.py
+++ test28.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 # @Date:   2022-09-02 14:15:39
-# @Last Modified time: 2022-09-02 21:13:08
+# @Last Modified time: 2022-09-03 20:57:30
 class people:
     def __init__(self):
         pass 
@@ -110,6 +110,130 @@ if __name__ == '__main__':
         print()
         for info in p.infos():
             print(info)
+
+
+
+
+
+
+from random import choice
+
+class ddivergence:
+
+    def __init__(self,name='robot'):
+        self.name = name 
+        pass 
+
+    def getChoice(self,name=None):
+        if name is not None:
+            self.name = name 
+        self.__lists = ['yes','no']
+        result = choice(self.__lists)
+        print(self.name,':yeah',resultb)
+
+
+
+class Point:
+    def __init__(self,x,y,z):
+        self.x = x 
+        self.y = y 
+        self.z = z 
+
+
+import abc
+class shuiguo(metaclass=abc.ABCMeta):
+    
+    all_type='sg'
+
+    def name(self):
+        pass
+
+    def func(self):
+        pass
+
+
+class Apple(shuiguo): #子类继承抽象类，必须定义read和write方法
+    def name(self):
+        print('我是苹果')
+
+    def func(self):
+        print('好吃')
+
+
+    def func(self):
+        print('yunfeizhike')
+
+
+apple =Apple()
+
+
+apple.func()
+
+
+print(apple.all_type)
+
+
+import abc 
+class BasePoint:
+    def __init__(self, x, y, z) -> None:
+        self.x = x 
+        self.y = y 
+        self.z = z 
+    @abc.abstractmethod
+    def dot(self, right):
+        pass
+class Point(BasePoint):
+    def __init__(self, x, y, z) -> None:
+        super().__init__(x, y, z)
+
+    def dot(self, right):
+        return self.x*right.x+self.y*right.y+self.z*self.z
+
+if __name__ == '__main__':
+    p1 = Point(0, 1, 2)
+    p2 = Point(2, 4, 6)
+    p1 = BasePoint(0, 1, 2)
+    p2 = BasePoint(2, 4, 6)
+    assert p1.dot(p2) is None
+
+
+
+class Apple():
+    def name(self,name):
+        self.__name = name 
+
+    def getName(self):
+        return self.__name
+
+class Apple():
+    def __setAge(self,age):
+        self.__age = age 
+
+    def getName(self):
+        return self.__name 
+
+    def info(self,age):
+        self.__setAge(age);
+   
+
+def getAge(self):
+    return self.__age
+
+
+
+def info(self,age):
+    if age < 1:
+       raise ValueError('too small')
+    elif age > 100:
+       raise ValueError('too big')
+    else:
+      self.__age = age
+
+
+
+
+
+
 
 
 
