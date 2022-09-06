6eae0e9332f6cef0d3ba8579a6a404e9437333b1
sanyuyuyu <1733659500@qq.com>
2022-09-06 21:58:52 +0800

    test_20220906

---
+++ test31.py
@@ -0,0 +1,81 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Date:   2022-09-06 19:42:23
+# @Last Modified time: 2022-09-06 21:57:18
+from datetime import datetime, timedelta
+if __name__ == '__main__':
+    today = datetime.now()
+    print(today)
+    print(today.strftime("%d/%m/%y"))
+    print(today.strftime("%A %d %B %Y"))
+from datetime import *
+now = datetime.now()
+y = now + timedelta(days=-1)
+m = now + timedelta(days=1)
+
+
+from datetime import datetime 
+from time import time, mktime 
+
+class TimeSpan:
+    def __init__(self) -> None:
+        self.start = round(time() * 1000)
+ 
+
+    def elapse_mill_secs(self):
+        end = round(time() * 1000)
+        return end-self.start
+
+    def elapse_secs(self):
+        return (self.elapse_mill_secs())/1000
+
+if __name__ == '__main__':
+    s = TimeSpan()
+
+    for i in range(0, 100000):
+        pass
+    print('耗时： {} 毫秒'.format(s.elapse_mill_secs()))
+    print('耗时： {} 秒'.format(s.elapse_secs()))
+
+
+from datetime import date, timedelta
+from calendar import monthrange
+ 
+def next_month(d):
+    offset = monthrange(d.year, d.month)
+    first_weeky_day, day_in_month = offset 
+    value = d+timedelta(days_in_month)
+    return value
+ 
+def for_each_month(start, finish, action):
+    while start < finish:
+        action(start)
+        start = next_month(start)
+ 
+if __name__ == '__main__':
+    for_each_month(
+        date(2008, 1, 1),
+        date.today(),
+        lambda d: print(d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
