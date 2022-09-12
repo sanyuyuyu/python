b42006ea7d5c4d11d9d81cc937ddca73cee14569
sanyuyuyu <1733659500@qq.com>
2022-09-12 22:33:29 +0800

    feature: test

--- 20220912.py
+++ 20220912.py
@@ -3,7 +3,7 @@
 # @Author: sanyuyuyu
 # @Date:   2022-09-12 20:28:32
 # @Last Modified by:   sanyuyuyu
-# @Last Modified time: 2022-09-12 22:24:31
+# @Last Modified time: 2022-09-12 22:32:23
 
 
 def test_git_push():
@@ -48,4 +48,27 @@ def get_data_from_file1(path: str) -> str:
 
 data = get_data_from_file1('data.txt')
 print(data)
+
+
+a = ['a','b','c']
+
+
+def get_abc(a):
+    ...
+    # 定义一个字符串
+    data = ''
+    # 遍历传进来的数组
+    for _ in a:
+        # 字符串相加
+        data += _
+    # 返回最终的数据
+    return data
+
+
+def get_abc1(a):
+    return ''.join(a)
+
+
+print(get_abc(a))
+print(get_abc1(a))
 
