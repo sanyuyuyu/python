0cb161c664be24792aa677b9bfb6cfee0cd7d58c
sanyuyuyu <1733659500@qq.com>
2022-09-13 22:19:17 +0800

    20220913

--- main/20220912.py
+++ main/20220912.py
@@ -1,59 +1,44 @@
-#! /usr/bin/env python3
-# -*- coding: utf-8 -*-
-# @Date:   2022-09-13 16:10:39
-# @Last Modified time: 2022-09-13 18:58:19
-def test_git_push():
-    """测试push提交."""
-    ...
-
-
-# def get_data_from_file(path: str) -> str:
-#     """获取path 例如:data.txt文件里的内容"""
-#     with open(path, 'r') as fp:
-#         data = fp.read()
-
-#     return data
-
-
-# path = '/Users/wangfeng/wss/python/data.txt'
-
-
-# data = get_data_from_file(path)
-# print(data)
-
-
 def get_data_from_file1(path: str) -> str:
     """获取path 例如:data.txt文件里的内容"""
 
+
     data = ''
     
-    with open(path, 'r') as file:
+    with open(path, 'r') as file:      
         for line in file.readlines():
-            if '111' in line:
-                data += line 
-                print(data)
+            #输出111.
+           if '111.\n' in line:
+            data += line 
+            print(data)
 
     return data       
           
-   
-   
-                
-            # print(line.strip())
+
+data = get_data_from_file1("data.txt")
 
 
 
-        # line = fp.readline()
-        # while line:
-        #     line = fp.readline()
-        #     if not line:
-        #         break
-        #     data += line
 
-    
+def get_data_from_file1(path: str) -> str:
+    data = ''
+    with open(path, 'r') as file:      
+        for line in file.readlines():
+            #输出111.
+           if line == '111.':
+            data += line 
+            print(data)
+
+    return data  
+
+data = get_data_from_file1("data.txt")
 
 
 
-print(data)
+
+
+
+
+
 
 
 
