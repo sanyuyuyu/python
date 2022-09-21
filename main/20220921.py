82d1d3dd912c170253b6a466b8b39df842d484fc
sanyuyuyu <1733659500@qq.com>
2022-09-21 21:57:54 +0800

    20220921

---
+++ main/20220921.py
@@ -0,0 +1,16 @@
+# /usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author: sanyuyuyu
+# @Date:   2022-09-21 20:47:38
+
+
+
+class Solution:
+    def getMaaximumGenrated(self, n: int) -> int:
+        if n == 0:
+            return 0
+        nums = [0] * (n + 1)
+        nums[1] = 1
+        for i in range(2, n + 1):
+            nums[i] = nums[i // 2] + i % 2 * nums[i // 2 + 1]
+        return max[nums]
