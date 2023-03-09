2f0c0f50976a5bfb4cab3cd090d200d5819f3331
sanyuyuyu <1733659500@qq.com>
2023-03-09 21:53:15 +0800

    20230309

---
+++ main/practice/Test_20230308.py
@@ -0,0 +1,97 @@
+'''
+print(dd)
+'''
+'''
+string = "renlele"
+print(string)
+list = [11,22,33]
+print(list)
+dictory = {"name:":"renlele","age:":1000,"sex:":"nan"}
+print(dictory)
+tuple = (38,"妇女节")
+print(tuple)
+tuple_2 = (38,{'Elric':'gang'})
+print(tuple_2)
+print("    *")
+print(" *******")
+print("*********")
+print("   ***  ")
+print("   ***  ")
+
+a = int(input("请输出一个整数:")) #input()函数以字符串返回
+b = int(input("请输出另一个整数"))
+print(a+b)
+'''
+print(30*"*")
+print("单号: ","DH201409230001")
+print("时间：","2014-09-23 08:56:14")
+print("名称","       数量 "," 单价 ","  金额 ")
+print("金士顿U盘  ","  1  "," 40.00", " 40.00")
+print("胜创16GTF卡","  1","   50.00", " 50.00")
+print("读卡器     ","  1","   8.00", "  8.00 ")
+print("网线2米     "," 1","   5.00","  5.00 ")
+print("总数：4","             总数: 103.00")
+print("折后总额:103.00")
+print("实收：103.00", "       找零：0.00")
+print("收银：管理员")
+print(" ")
+print(30*"*")
+print("         植树证书1   ")
+print("          谢谢你")
+print('''你于2019年11月3日申请种植的梭梭树
+已被中国扶贫基金会认领，将种植到武威
+地区。''')
+print("          树苗编号    ")
+print(" NO.HFK20308960305")
+'''
+r = int(input("半径r："))
+d = 2 * r
+S = 3.14*r*r
+print(S)
+print(d)
+'''
+
+total = 29.5
+car_1 = 4
+car_2 = 2.5
+print("运送次数",(29.5-car_1*4)/car_2)
+
+
+weight = float(input("weight:"))
+tall =  float(input("tall:"))
+BMI = weight/tall*tall
+print(BMI)
+
+
+
+C = int(input("摄氏度："))
+K = int(input("华氏度："))
+K2 = 32+C*1.8
+C2 = (K - 32) / 1.8
+print(C2)
+print(K2)
+
+
+K3 = float(input("开氏度："))
+CC = float(input("摄氏度: "))
+C3 = K3 + 273.15
+K4 = C3 -273.15
+print(C3)
+print(K4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
