7f5a6c92adfc8f802ef4bbd96a96dc621921dd54
sanyuyuyu <1733659500@qq.com>
2023-03-21 22:15:04 +0800

    Test_20230321

---
+++ main/practice/Test_20230321.py
@@ -0,0 +1,30 @@
+'''
+n = eval(input("请输入正整数:"))
+
+s = "{:->20,}".format(n)
+
+print(s)
+
+
+str = hs
+
+print(str.center(10,'*'))
+'''
+
+a = 10.5
+
+def bove_zero(t):
+
+	return a > 40
+
+	print(a)
+
+for x in range(0,3 ):
+
+	y = 0
+
+	y += x
+
+	print('y',y)
+	
+
