e2c86e397b29191085058baed327a8dc60db4ca1
sanyuyuyu <1733659500@qq.com>
2023-03-23 22:45:56 +0800

    20230323

---
+++ main/practice/Test_20230323.py
@@ -0,0 +1,8 @@
+a = 'python is a good biancheng ruanjian'
+print(a)
+print(a.split()) #分割字符串a，并以列表类型返回
+print(a.join('abcdefg'))
+print(a.strip('p'))
+print(a.count('n'))
+print(a.lower())
+print(a.upper())
