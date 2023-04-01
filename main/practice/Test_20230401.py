35fb618e1a2d67bedb2984abe36c5bd4df733506
sanyuyuyu <1733659500@qq.com>
2023-04-01 22:08:17 +0800

    20230401

---
+++ main/practice/Test_20230401.py
@@ -0,0 +1,15 @@
+'''
+import turtle
+for i in range(4):
+	turtle.fd(100)
+	turtle.fd(-100)
+	turtle.seth((i+1)*90)
+'''
+for i in "Summer":
+	if i == 'm':
+		break
+	print(i,end='')
+
+for num in range(1,4):
+	num *= num
+	print(num,end='')
