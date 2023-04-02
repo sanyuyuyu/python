ef930270f1a4754e4b7caef473e9fcfb92f102f6
sanyuyuyu <1733659500@qq.com>
2023-04-02 22:13:14 +0800

    20230402

---
+++ main/practice/Test_20230402.py
@@ -0,0 +1,19 @@
+
+
+fi = open("aaa.txt","r",encoding="utf-8")
+fo = open("aaa.txt", "w", encoding="utf-8")
+flag = False
+for line in fi:
+    if "【原文】" in line:
+        flag = True
+        continue
+    if "【注释】" in line:
+        flag = False
+    line = line.strip("\n")
+    line = line.lstrip().rstrip()
+    if flag:
+        print(line)
+        if line:
+        	fo.write(line+"\n")
+fo.close()
+fi.close()     
