a8f01f05dd629001745a2129d3a3b3483cc0d1d9
sanyuyuyu <1733659500@qq.com>
2023-04-07 22:33:56 +0800

    20230407

--- main/practice/Test_20230406.py
+++ main/practice/Test_20230406.py
@@ -7,7 +7,7 @@ f.write('wss')
 print(ls)
 f.close()
 
-data = input("请输入姓名")
+data = input("请输入姓名: ")
 num = 0
 age = 0
 number = 0
@@ -20,3 +20,29 @@ while data:
 	data = input()
 age = age / num
 print('平均年龄{:.2f},男性人数是{}'.format(age,number))
+
+'''
+
+键盘输入一组人员的姓名、性别、年龄等信息,信息间采用空格分隔,每人一行,空行回车结束录入,示例格式如下:
+张三 男 23
+李四 女 21
+王五 男 18
+计算并输出这组人员的平均年龄(保留2位小数)和其中男性人数,格式如下:
+平均年龄是20.67 男性人数是2
+'''
+data = input()
+
+name = 0
+male = 0
+age = 0
+while data:
+	
+
+
+print('平均年龄{:.2f},男性人数是{}'.format())
+
+
+
+
+
+
---
+++ main/practice/Test_20230407.py
@@ -0,0 +1,42 @@
+'''问题1(5分): 对“命运.txt"文件进行字符频次统计,输出频次最高的中文字符(不包含标点符号)
+及其频次,字符与频次之间采用英文冒
+号”:"分隔,示例格式如下:'''
+
+file = open("命运.txt", "r", encoding="utf-8")
+info = file.read()
+d = {}
+for s in info:
+    if s not in "，。？《》！【】“”‘’、":
+        d[s] = d.get(s, 0 ) + 1
+arr = list(d.items())
+arr.sort(key=lambda item:item[1], reverse=True)
+file.close()
+print("{}:{}".format(arr[0][0],arr[0][1]))
+
+
+'''
+file = open('命运.txt','r',encoding = 'utf-8')
+info = file.read()
+d = {}
+for s in info:
+    if s not in ',，。？《》！【】“”‘’、':
+        d[s] = d.ger(s,0) + 1
+arr = list(d.items())
+arr.sort(key = lambda item:item[1],reverse = True)
+file.close()
+print('{}:{}'.format(arr[0][0],arr[0][1]))
+
+'''
+
+
+file = open("命运.txt", "r", encoding="utf-8")
+info = file.read()
+d = {}
+for s in info:
+    if s not in "\n":
+        d[s] = d.get(s, 0) + 1
+file.close()
+ls = list(d.items())
+ls.sort(key=lambda item:item[1], reverse=True)
+for i in range(10):
+    print("{}".format(ls[i][0]), end="")
