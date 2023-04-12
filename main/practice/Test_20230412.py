ca6b13d00ebb574d523f3f7ad3878593708c6b9e
sanyuyuyu <1733659500@qq.com>
2023-04-12 21:49:05 +0800

    20230412

---
+++ main/practice/Test_20230412.py
@@ -0,0 +1,73 @@
+# sum = 0
+# for i in range(1,101):
+# 	sum += i
+# print(sum)
+
+# sum = 0
+# for i in range(1,113,3):
+# 	sum += i
+# print(sum)
+
+# sum = 0
+# for i in range(5,101,5):
+# 	sum += i
+# print(sum)
+
+
+# a = int(input('输入a:'))
+# b = int(input('输入b:'))
+
+# while(a):
+# 	if a < b:
+# 		t = a 
+# 		a = b 
+# 		b = t 
+# 	a %= b 
+# print(b)
+
+
+
+# n = int(input())
+
+# for i in range(30):
+# 	if str(n) == str(n)[::-1]:
+# 		print(i)
+# 		break
+# 	else:
+# 		n = n + int(str(n)[::-1])
+# else:
+# 	print(30)
+
+
+
+# year , time = map(float,input().split())
+
+# if time >= 5:
+# 	if time > 40:
+# 		print(40*50 + (time - 40)*1.5*50)
+# 	else:
+# 		print(50*time)
+# if time < 5:
+# 	if time < 40:
+# 		print(40 * 40 + (time - 40)*1.5*30)
+# 	else:
+# 		print(30 * time)
+
+
+# incount = int(input())
+
+# if incount < 36000:
+# 	print(incount -  incount * 0.03)
+# elif incount > 36000 and incount < 144000:
+# 	print(incount - 36000 * 0.03 - (incount - 36000) * 0.1)
+# elif incount >144000 and incount <300000:
+# 	print(incount - 36000 * 0.03 - (incount - 144000)* 0.2 )
+# else:
+# 	print(incount - 36000 * 0.03 - (incount - 300000) * 0.25)
+
+
+
+a,b,c = map(int, input().split())
+
+ls = list(map(float,input().split()))
+
