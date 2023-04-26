eecd8abe9ca718d386f3f87659af329a9b5c9935
sanyuyuyu <1733659500@qq.com>
2023-04-26 22:22:57 +0800

    20230426

---
+++ main/practice/Test_20230426.py
@@ -0,0 +1,80 @@
+# test = ("a","b","c","a","c") 
+# result = []
+# for s in test:
+# 	cnt = test.count(s)
+# 	tmp = (s,cnt)
+# 	if tmp in result:
+# 		continue
+# 	else:
+# 		result.append(tmp)
+# print(result)
+
+
+# users = []
+# i = 0 
+# while i < 3:
+# 	name = input('请输入姓名：')
+# 	age = int(input('请输入年龄'))
+# 	users.append({'name':name,'age':age})
+# for thnum,user in enumerate(users,start = 1):
+# 	print(f"{thnum} {user['name']} {user['age']}")
+
+# thnum = 1
+
+# for user in users:
+# 	print(f'{thnum} {user['name']}{user['age']}')
+# 	thnum+=1
+
+
+
+# def sum_test(n):
+# 	sum = 0 
+# 	i = 1
+# 	while i <= n:
+# 		sum += i 
+# 		i += 1
+# 	print(sum)
+# num = 10
+# sum_test(num)
+
+
+
+# def max_min(*args):
+# 	max = args[0]
+# 	min = args[0]
+# 	for num in args:
+# 		if max < num:
+# 			max = num
+# 		if min > num:
+# 			min = num
+# 	return max,min
+# max,min = max_min(1,2,35,6,7,8,9,1)
+# print(f'max = {max},min = {min}')
+
+
+# a = 'abcdefg'
+# s = a[::-1]
+# print(s)
+
+a = 'abcd'
+index = len(a) - 1
+while lg > 0:
+	print(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
