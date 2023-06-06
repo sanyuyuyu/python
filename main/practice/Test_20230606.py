f0122e1b7e82181ef853164f7c7f49d3e4f86683
sanyuyuyu <1733659500@qq.com>
2023-06-06 22:52:27 +0800

    20230606

---
+++ main/practice/Test_20230606.py
@@ -0,0 +1,46 @@
+# def power(x,n = 2):
+# 	s = 1
+# 	while n > 0:
+# 		n = n - 1
+# 		s = s * x
+# 	return s
+# def enroll(name,gender):
+# 	print('name:',name)
+# 	print('gender:',gender)
+
+# def enroll(name,gender,age = 6,city='zhejiang'):
+# 	print('name:',name)
+# 	print('gender:',gender)
+# 	print('age:',age)
+# 	print('city',city)
+
+# def add_end(L = []):
+# 	L.append('END')
+# 	return L
+
+# def add_end(L = None):
+# 	if L is None:
+# 		L = []
+# 	L.append('END')
+# 	return L 
+
+
+# def calc(numbers):
+# 	sum = 0 
+# 	for n in numbers:
+# 		sum = sum + n * n 
+# 	return sum
+
+# def calc(*numbers):
+# 	sum = 0
+# 	for n in numbers:
+# 		sum = sum + n * n 
+# 	return sum
+
+# def mul(x,*y):
+	# # sum = x 
+	# for n in y:
+	# 	sum = sum * n 
+	# return sum
+
+
