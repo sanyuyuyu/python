c9173b9466b96da5365cfae4808c878d66fe39c0
sanyuyuyu <1733659500@qq.com>
2023-06-17 22:30:34 +0800

    20230617

---
+++ main/practice/Test_20230617.py
@@ -0,0 +1,65 @@
+# class Student(object):
+# 	def __init__(self, name, score):
+#         self.name = name
+#         self.score = score
+
+    # def print_score(self):
+    #     print('%s: %s' % (self.name, self.score
+
+# class Student(object):
+#     def __init__(self, name, score):
+#         self.name = name
+#         self.score = score
+
+#     def get_grade(self):
+#         if self.score >= 90:
+#             return 'A'
+#         elif self.score >= 60:
+#             return 'B'
+#         else:
+#             return 'C'
+
+# lisa = Student('Lisa', 99)
+# bart = Student('Bart', 59)
+# print(lisa.name, lisa.get_grade())
+# print(bart.name, bart.get_grade())
+
+
+# import random 
+# class Fish():
+#     def __init__(self,x,y):
+#         self.x=random.randint(0,10)
+#         self.y=random.randint(0,10)
+
+# d=Fish()
+# print(d.x)
+
+
+# li_one = [1,2,1,2,3,5,4,3,5,7,4,7,8]
+# new_li =[]
+# for i in li_one:
+#     if i not in new_li:
+#         new_li.append(i)
+# print(new_li)
+
+
+li_one = [1,2,1,2,3,5,4,3,5,7,4,7,8]
+li = []
+for i in li_one:
+	if i not in li:
+		li.append(i)
+print(li)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
