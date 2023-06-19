a779ca5c384a0909dfa2a1cd5643ce478d644511
sanyuyuyu <1733659500@qq.com>
2023-06-19 23:00:30 +0800

    20230619

---
+++ main/practice/Test_20230619.py
@@ -0,0 +1,41 @@
+# class Animal(object):
+#     def run(self):
+#         print('Animal is running...')
+
+
+# class Dog(Animal):
+#     pass
+
+# class Cat(Animal):
+#     pass
+
+# dog = Dog()
+# dog.run()
+
+# cat = Cat()
+# cat.run()
+
+# class Dog(Animal):
+#     def run(self):
+#         print('Dog is running...')
+
+#     def eat(self):
+#         print('Eating meat...')
+
+# class Dog(Animal):
+#     def run(self):
+#         print('Dog is running...')
+
+# class Cat(Animal):
+#     def run(self):
+#         print('Cat is running...')
+
+# def run_twice(animal):
+#     animal.run()
+#     animal.run()
+
+
+# class Tortoise(Animal):
+#     def run(self):
+#         print('Tortoise is running slowly...')
+
