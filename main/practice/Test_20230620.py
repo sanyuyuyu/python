bf78a4934ebe5418cf3bfcde29f58d42d7304185
sanyuyuyu <1733659500@qq.com>
2023-06-20 21:21:53 +0800

    20230620

---
+++ main/practice/Test_20230620.py
@@ -0,0 +1,61 @@
+#偶数和
+# def odd():
+# 	result = 0
+# 	for i in range(1,101):
+# 		if i % 2 == 0:	
+# 			result += i 
+# 	print(result)
+# odd()
+
+# def num_odd():
+# 	result = 0
+# 	counter = 1 
+# 	while counter <= 100:
+# 		counter += 1
+# 		if counter % 2 == 1:
+# 			coutinue
+# 		result += counter
+# 	return result
+# print(num_odd)
+
+
+
+# def odd():
+# 	result = 0 
+# 	for i in range(1,101):
+# 		if i % 2 == 0:
+# 			result += i 
+# 	return result
+# print(odd())
+
+#删除列表重复功能
+# li_one = [1,2,1,2,3,5,4,3,5,7,4,7,8]
+# li_kong = []
+# for i in li_one:
+# 	if i not in li_kong:
+# 		li_kong.append(i)
+# print(li_kong)
+
+
+# li_one = [1,2,1,2,3,5,4,3,5,7,4,7,8]
+# print(list(set(li_one))) #set空集合
+
+#递归法
+# def fact(num):
+# 	if num == 2:
+# 		return 1
+# 	else:
+# 		return num * fact(num - 1)
+# print(fact(20))
+
+#文本文件与二进制文件的区别：根据逻辑结构不同，计算机中文件可以分为文本文件和二进制文件
+#文本文件主要存储文本字符等数据，二进制文件主要存储音频、视频、图像等数据
+
+#参数混合传递规则
+#位置参数传递、关键字参数传递、默认值参数传递、打包传递方式传递
+
+#三种格式符
+#% format()、values、%
+#format str.format(values)
+#f-string 
+
