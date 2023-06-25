5ed4c8665affd1a07cb551ff7f459060af4dc3ee
sanyuyuyu <1733659500@qq.com>
2023-06-25 23:29:38 +0800

    20230625

---
+++ main/practice/Test_20230625.py
@@ -0,0 +1,22 @@
+# class Student(object):
+
+#     def get_score(self):
+#          return self._score
+
+#     def set_score(self, value):
+#         if not isinstance(value, int):
+#             raise ValueError('score must be an integer!')
+#         if value < 0 or value > 100:
+#             raise ValueError('score must between 0 ~ 100!')
+#         self._score = value
+
+# class Student(object):
+
+#     def birth(self):
+#         return self._birth
+
+#     def birth(self, value):
+#         self._birth = value
+
+#     def age(self):
+#         return 2015 - self._birth
